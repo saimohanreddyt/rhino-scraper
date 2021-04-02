01cf445288336e74f8ea02fc188ebe4dde6736b1
saimohanreddyt <tsaimohanreddy@gmail.com>
2021-04-02 10:08:08 +0530

    scraping

---
+++ main.go
@@ -0,0 +1,43 @@
+//Intiliaze go mod @modules so we can handle dependecies in our current project
+//go get github.com/gocolly/colly
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+type Fact struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+}
+
+func main() {
+	allFacts := make([]Fact, 0)
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("factretriever.com", "www.factretriever.com"),
+	)
+	collector.OnHTML("factsList li", func(element *colly.HTMLElement) {
+		factId, err := strconv.Atoi(element.Attr("id"))
+		if err != nil {
+			log.Println("Could not get id")
+		}
+		factDesc := element.Text
+
+		fact := Fact{
+			ID:          factId,
+			Description: factDesc,
+		}
+		allFacts = append(allFacts, fact)
+	})
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+	collector.Visit("https://www.factretriever.com/dog-facts")
+}
